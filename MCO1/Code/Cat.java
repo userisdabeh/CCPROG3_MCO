10c96c984492feab9370d522a4187e711e566c77
userisdabeh <120602326+userisdabeh@users.noreply.github.com>
2025-03-10 04:15:27 +0800

    added javadoc

--- MCO1/Code/Cat.java
+++ MCO1/Code/Cat.java
@@ -5,9 +5,24 @@
  * Currently, the Cat class is not implemented and always returns false.
  */
 public class Cat extends Piece {
+
+    /**
+     * Constructor for the Cat class at the specified position.
+     * @param x The row position of the cat on the board.
+     * @param y The column position of the cat on the board,
+     */
     public Cat(int x, int y) {
         super("Cat", 2, x, y);
     }
+
+    /**
+     * Attempts to move the Cat to the new position.
+     * 
+     * @param newX The new row position of the Cat.
+     * @param newY The new column position of the Cat.
+     * @param board The game board.
+     * @return false since the Cat cannot yet move.
+     */
     @Override
     public boolean move(int newX, int newY, Board board) {
         return false;
