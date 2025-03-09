05f6ffdd8ad2ce81096b4983a22c1dac262fad87
userisdabeh <120602326+userisdabeh@users.noreply.github.com>
2025-03-10 04:15:01 +0800

    added javadoc

--- MCO1/Code/Cat.java
+++ MCO1/Code/Cat.java
@@ -1,3 +1,9 @@
+/**
+ * Represents the Cat piece in the game.
+ * The Cat has a strength of 2 and is a subclass of Piece.
+ * 
+ * Currently, the Cat class is not implemented and always returns false.
+ */
 public class Cat extends Piece {
     public Cat(int x, int y) {
         super("Cat", 2, x, y);
