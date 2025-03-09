bcaaddd101050f67de0f7ed4c54491b5c066768b
userisdabeh <120602326+userisdabeh@users.noreply.github.com>
2025-03-10 04:06:30 +0800

    added javadoc

--- MCO1/Code/Wolf.java
+++ MCO1/Code/Wolf.java
@@ -1,7 +1,28 @@
+/**
+ * Represents the Wolf piece in the game.
+ * The Wolf has a strength of 4 and is a subclass of Piece.
+ * 
+ * Currently, the Wolf class is not implemented and always returns false.
+ */
 public class Wolf extends Piece {
+
+    /**
+     * Constructor for the Wolf class at the specified position.
+     * @param x The row position of the wolf on the board.
+     * @param y The column position of the wolf on the board,
+     */
     public Wolf(int x, int y) {
         super("Wolf", 4, x, y);
     }
+
+    /**
+     * Attempts to move the Wolf to the new position.
+     * 
+     * @param newX The new row position of the Wolf.
+     * @param newY The new column position of the Wolf.
+     * @param board The game board.
+     * @return false since the Wolf cannot yet move.
+     */
     @Override
     public boolean move(int newX, int newY, Board board) {
         return false;
