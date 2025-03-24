5f74e4a41e6fb704b782891a5053919769250d35
userisdabeh <120602326+userisdabeh@users.noreply.github.com>
2025-03-24 23:01:26 +0800

    changed

--- MCO2/GamePanelView.java
+++ MCO2/GamePanelView.java
@@ -4,11 +4,13 @@ import javax.swing.*;
 public class GamePanelView{
 
     private JLabel currentPlayerLabel = new JLabel("Current Player: ");
-    private JLabel playerName = new JLabel("I work here");
+    private JLabel playerName = new JLabel("Player 1");
     private JPanel gameP = new JPanel();
     private JPanel boardP = new JPanel();
     public JPanel mainPanel = new JPanel();
-    private JPanel[] board = new JPanel[63];
+    private JButton[] board = new JButton[63];
+
+    ImageIcon tigerGreen = new ImageIcon(getClass().getResource("./img/tiger-green.png"));
 
     public GamePanelView (){
         mainPanel.setLayout(new BorderLayout());
@@ -24,17 +26,18 @@ public class GamePanelView{
         playerName.setAlignmentX(Component.CENTER_ALIGNMENT);
         playerName.setForeground(Color.decode("#000000"));
 
-        gameP.add(Box.createRigidArea(new Dimension(0, 100)));
+        gameP.add(Box.createRigidArea(new Dimension(0, 15)));
         gameP.add(currentPlayerLabel);
-        gameP.add(Box.createRigidArea(new Dimension(0, 90)));
+        gameP.add(Box.createRigidArea(new Dimension(0, 10)));
         gameP.add(playerName);
+        gameP.add(Box.createRigidArea(new Dimension(0, 15)));
 
         boardP.setLayout(new GridLayout(9, 7));
         for(int i = 0; i < 63; i++) {
-            board[i] = new JPanel();
-            board[i].setBackground(Color.decode("#B2FBA5"));
-            board[i].setBorder(BorderFactory.createLineBorder(Color.BLACK));
-            boardP.add(board[i]);
+            JButton tile = new JButton();
+            board[i] = tile;
+            boardP.add(tile);
+            tile.setIcon(tigerGreen);
         }
 
         mainPanel.add(gameP, BorderLayout.NORTH);
